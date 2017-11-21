ef302b456a559e1b75f774d2f7ccdbf78e1b6473
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-11-21 17:24:17 +0800

    Implement page/tag iterator

--- utoolbox/demo/load_tiff.py
+++ utoolbox/demo/load_tiff.py
@@ -1,5 +1,12 @@
 from utoolbox.io import imopen
+from timeit import default_timer as timer
+
+from pprint import pprint
 
 file_path = 'data/RAWtan1_3_3DSIMb_ch1_stack0001_561nm_0019400msec_0000215229msecAbs.tif'
-with imopen(file_path, 'r') as fd:
-    pass
+
+start = timer()
+with imopen(file_path, 'r') as imfile:
+    pprint(imfile[0].tags)
+end = timer()
+print('image scanned in {:.3f}s'.format(end - start))
