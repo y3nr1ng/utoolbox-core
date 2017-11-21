3cf5d359e990f717f177a5c96d7add2687b8a824
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-11-21 18:59:05 +0800

    Add image type hierarchy

--- utoolbox/demo/load_tiff.py
+++ utoolbox/demo/load_tiff.py
@@ -3,10 +3,15 @@ from timeit import default_timer as timer
 
 from pprint import pprint
 
-file_path = 'data/RAWtan1_3_3DSIMb_ch1_stack0001_561nm_0019400msec_0000215229msecAbs.tif'
+#file_path = 'data/RAWtan1_3_3DSIMb_ch1_stack0001_561nm_0019400msec_0000215229msecAbs.tif'
+file_path = '/Users/Andy/Downloads/min_data.tif'
 
 start = timer()
+
 with imopen(file_path, 'r') as imfile:
-    pprint(imfile[0].tags)
+    for page in imfile:
+        pprint(page.tags)
+        print(page.rasters)
+
 end = timer()
 print('image scanned in {:.3f}s'.format(end - start))
