1b6f36e472269b8ea74dceaa3aac0a3ec18c7cce
Liu Yen-Ting <windows.linux.mac@gmail.com>
2017-11-22 11:22:59 +0800

    Try to accomodate field type conversion

--- utoolbox/demo/load_tiff.py
+++ utoolbox/demo/load_tiff.py
@@ -1,8 +1,14 @@
 from utoolbox.io import imopen
-from utoolbox.io.codecs.tiff.tags import TAG_NAME
+from utoolbox.io.codecs.tiff.tags import TagName, TagType
 from timeit import default_timer as timer
 
 from pprint import pprint
+
+print(TagType['Rational'])
+print(TagType.Rational)
+print(TagType((1, 'B')))
+
+raise RuntimeError('PAUSE')
 
 file_path = 'data/RAWtan1_3_3DSIMb_ch1_stack0004_561nm_0077599msec_0000273428msecAbs.tif'
 #file_path = '/Users/Andy/Downloads/min_data.tif'
@@ -12,7 +18,7 @@ start = timer()
 with imopen(file_path, 'r') as imfile:
     for page in imfile:
         for tag_id, tag_info in page.tags.items():
-            print('{}: {}'.format(TAG_NAME.get(tag_id, '<Unknown>'), tag_info))
+            print('{}: {}'.format(TagName[tag_id].name, tag_info))
         print(page.rasters)
         print()
 
