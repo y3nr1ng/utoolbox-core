8d942789d29db46a8071d4178a522c600548130a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-01-16 22:24:46 +0800

    Connect codec with abstract volume

--- utoolbox/io/primitives/volume.py
+++ utoolbox/io/primitives/volume.py
@@ -1,11 +1,53 @@
+import logging
+logger = logging.getLogger(__name__)
+
+import os.path
+
 import numpy as np
 
+from ..operations import imopen
+
 class SimpleVolume(np.ndarray):
-    def __new__(subtype, *args, **kwargs):
-        # create SimpleVolume with the usual ndarray input arguments
-        # NOTE SimpleVolume.__array_finalize__ is triggered
-        obj = super(SimpleVolume, subtype).__new__(subtype, args, kwargs)
-        #TODO set new attributes
+    def __new__(subtype, file_path=None, shape=None):
+        """
+        Create ndarray instance of SimpleVolume, given the usual arguments.
+
+        Parameters
+        ----------
+        file_path: string
+            path to the image
+        shape: tuple of integers
+            shape of the volumetric data
+
+        Returns
+        -------
+        SimpleVolume
+            newly created volume object, child class of np.ndarray
+        """
+        if file_path:
+            if os.path.exists(file_path):
+                # read from file
+                with imopen(file_path, 'r') as imfile:
+                    shape = imfile.shape
+                    dtype = imfile.dtype
+                    obj = super(SimpleVolume, subtype).__new__(subtype,
+                                                               shape=shape,
+                                                               dtype=dtype)
+                    print('before loading')
+                    for index, page in imfile:
+                        print(index)
+                        obj[..., index] = page.raster
+                    print('loaded')
+            elif shape:
+                raise NotImplementedError("Create new file is not implemented.")
+                obj = super(SimpleVolume, subtype).__new__(subtype,
+                                                           shape=shape)
+            else:
+                raise ValueError("'shape' must be provided to allocate spaces.")
+        else:
+            raise ValueError("'file_path' must be provided.")
+
+        obj._path = path
         return obj
 
     def __array_finalize__(self, obj):
@@ -15,18 +57,18 @@ class SimpleVolume(np.ndarray):
         gave it.
         """
         # from explicit constructor
-        #   in the middle of SimpleVolume.__new__ construcotr
+        #   in the middle of SimpleVolume.__new__ constructor
         if obj is None:
             return
 
         # from view casting
         #   obj is an array, type(obj) can be SimpleVolume
 
+
         # from new-from-template, such as slicing
         #   type(obj) is SimpleVolume
 
-        #TODO do additional processing if required
-    
+
     def __init__(self, path):
         self._path = path
 
