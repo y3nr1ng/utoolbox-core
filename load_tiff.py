ea1a4eb8c4876256053ebf3939a01902940f4fc5
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-01-16 16:54:15 +0800

    Basic grayscale loader

--- load_tiff.py
+++ load_tiff.py
@@ -1,22 +1,31 @@
+from timeit import default_timer as timer
+from pprint import pprint
+
+import matplotlib.pyplot as plt
+
 from utoolbox.io import imopen
 from utoolbox.io.codecs.tiff.tags import Tags
-from timeit import default_timer as timer
-
-from pprint import pprint
 
 file_path = 'data/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs_decon.tif'
 
 start = timer()
 
 with imopen(file_path, 'r') as imfile:
+    h = None
     for page in imfile:
-        for tag, tag_info in page.tags.items():
-            print('{} ({}): {}'.format(tag, tag.value, tag_info))
-        print(page.raster)
-        print()
+        #for tag, tag_info in page.tags.items():
+        #    print('{} ({}): {}'.format(tag, tag.value, tag_info))
+        if h is None:
+            h = plt.imshow(page.raster)
+            plt.ion()
+            plt.show()
+        else:
+            h.set_data(page.raster)
+            plt.draw()
+        plt.pause(0.001)
 
         # only test the first page
-        break
+        #break
 
 end = timer()
 print('image scanned in {:.3f}s'.format(end - start))
