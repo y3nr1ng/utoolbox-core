2dd2ac5a8e7f9ced4a58e047e9ca575c36df09f2
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-01-16 22:23:37 +0800

    Adjust template to include basic properties

--- utoolbox/io/codecs/template.py
+++ utoolbox/io/codecs/template.py
@@ -7,3 +7,27 @@ class FileIO(object):
 
     def __exit__(self, exc_type, exc_value, traceback):
         raise NotImplementedError
+
+    def __len__(self):
+        """Return number of subfiles."""
+        raise NotImplementedError
+
+    def __iter__(self):
+        raise TypeError("Not supported.")
+
+    def __next__(self):
+        raise TypeError("Not supported.")
+
+    def __getitem__(self, index):
+        raise TypeError("Not supported.")
+
+    def __setitem__(self, index, data):
+        raise TypeError("Not supported.")
+
+    @property
+    def shape(self):
+        raise NotImplementedError
+
+    @property
+    def dtype(self):
+        raise NotImplementedError
