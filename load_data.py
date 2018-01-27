583a5139d630be81eef140dbd04b99603c02e32e
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-01-27 12:06:56 +0800

    Remove yt code

--- load_data.py
+++ load_data.py
@@ -2,8 +2,6 @@ import boltons.debugutils
 #boltons.debugutils.pdb_on_exception()
 
 import numpy as np
-import yt
-yt.toggle_interactivity()
 
 from utoolbox.io import TimeSeries
 import utoolbox.io.primitives as dtype
@@ -19,16 +17,4 @@ def load_file():
     return dtype.SimpleVolume(file_path)
 
 data = load_file()
-#renderer = VolumeRender(title='demo', data=data)
-
-units = yt.units.unit_registry.UnitRegistry()
-print(units.keys())
-
-# associate a field
-data = dict(density=(data, 'au'))
-bbox = np.array([[-1.5, 1.5], [-1.5, 1.5], [-1.5, 1.5]])
-ds = yt.load_uniform_grid(data, data['density'][0].shape, length_unit="Mpc", bbox=bbox, nprocs=64)
-
-sc = yt.create_scene(ds)
-sc.show()
-#sc.save('rendering.png')
+renderer = VolumeRender(title='demo', data=data)
