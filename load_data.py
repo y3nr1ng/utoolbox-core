f0458f669ef1e38736c0865ab420849603a054f3
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-02-01 11:45:01 +0800

    Use imageio instead of custom loader One shall not reinvent the wheels.

--- load_data.py
+++ load_data.py
@@ -1,20 +1,17 @@
 import boltons.debugutils
-#boltons.debugutils.pdb_on_exception()
+boltons.debugutils.pdb_on_exception()
 
 import numpy as np
 
-from utoolbox.io import TimeSeries
-import utoolbox.io.primitives as dtype
 from utoolbox.utils.decorators import timeit
-
-#ts = TimeSeries(dtype.SimpleVolume, folder='data')
-#print('{} time points'.format(len(ts)))
 
 file_path = 'data/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs_decon.tif'
 
 @timeit
 def load_file():
+    import utoolbox.io.primitives as dtype
     return dtype.SimpleVolume(file_path)
 
 data = load_file()
-renderer = VolumeRender(title='demo', data=data)
+print(data.shape)
+print(type(data))
--- utoolbox/io/primitives/volume.py
+++ utoolbox/io/primitives/volume.py
@@ -4,74 +4,33 @@ logger = logging.getLogger(__name__)
 import os.path
 
 import numpy as np
-
-from utoolbox.io.operations import imopen
+import imageio
 
 class SimpleVolume(np.ndarray):
-    def __new__(subtype, file_path=None, shape=None):
-        """
-        Create ndarray instance of SimpleVolume, given the usual arguments.
+    def __new__(cls, path=None, array=None, shape=None, dtype=None):
+        """SimpleVolume(path=None, array=None, shape=None, dtype=None)
+
+        A subclass of np.ndarray that acts as the common ground for volumetric
+        dataset.
 
         Parameters
         ----------
-        file_path: string
-            path to the image
-        shape: tuple of integers
-            shape of the volumetric data
-
-        Returns
-        -------
-        SimpleVolume
-            newly created volume object, child class of np.ndarray
         """
-        if file_path:
-            if os.path.exists(file_path):
-                # read from file
-                with imopen(file_path, 'r') as imfile:
-                    shape = imfile.shape
-                    dtype = imfile.dtype
-                    obj = super(SimpleVolume, subtype).__new__(subtype,
-                                                               shape=shape,
-                                                               dtype=dtype)
-                    for index, page in enumerate(imfile):
-                        obj[..., index] = page.raster
-            elif shape:
-                raise NotImplementedError("Create new file is not implemented.")
-                obj = super(SimpleVolume, subtype).__new__(subtype,
-                                                           shape=shape)
-            else:
-                raise ValueError("'shape' must be provided to allocate spaces.")
-        else:
-            raise ValueError("'file_path' must be provided.")
-
-        obj._path = file_path
+        if path is None and array is None:
+            # create the ndarray instance of our type
+            obj = super(SimpleVolume, cls).__new__(cls,
+                                                   shape=shape, dtype=dtype)
+            return obj
+        elif path:
+            if array is not None:
+                logger.warning('\'array\' is shadowed by \'path\', ignored.')
+            array = imageio.volread(path)
+        # simple view casting
+        obj = array.view(cls)
         return obj
 
     def __array_finalize__(self, obj):
-        """
-        `self` is a new object resulting from ndarray.__new__(SimpleVolume, ...)
-        Therefore, it only has attributes that the ndarray.__new__ constructor
-        gave it.
-        """
-        # from explicit constructor
-        #   in the middle of SimpleVolume.__new__ constructor
-        if obj is None:
-            #TODO load data here
-            print('obj is None')
-            return
-
-        # from view casting
-        #   obj is an array, type(obj) can be SimpleVolume
-
-
-        # from new-from-template, such as slicing
-        #   type(obj) is SimpleVolume
-
-
-    def __init__(self, path):
-        self._path = path
-
-    def __del__(self):
+        # fill in default metadata here
         pass
 
 class SIVolume(object):
