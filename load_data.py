556b478f9828da7234ea04a4dca6d3b520052e93
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-02-02 14:29:16 +0800

    Add MultiVolume test script

--- load_data.py
+++ load_data.py
@@ -1,9 +1,12 @@
 import boltons.debugutils
 boltons.debugutils.pdb_on_exception()
 
+from vispy import app, scene
+
 import numpy as np
 
 from utoolbox.utils.decorators import timeit
+from utoolbox.viewers.volume import MultiVolume
 
 file_path = 'data/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs_decon.tif'
 
@@ -13,5 +16,20 @@ def load_file():
     return dtype.SimpleVolume(file_path)
 
 data = load_file()
-print(data.shape)
-print(type(data))
+
+# create Vispy environment
+canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
+view = canvas.central_widget.add_view()
+
+# attach visual node
+#TODO create volumes (data, clim, cmap) tuples
+volumes = []
+vol_node = MultiVolume(volumes, parent=view.scene)
+vol_node.transform = scene.STTransform(translate=(64, 64, 0))
+
+view.camera = scene.cameras.TurntableCamera(parent=view.scene, fov=60.)
+
+canvas.update()
+
+if __name__ == '__main__':
+    app.run()
