121f5fb7695dc3ed6e0150e1700ce477c3bc2a00
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-02-02 14:43:00 +0800

    Use official colormap

--- load_data.py
+++ load_data.py
@@ -2,6 +2,7 @@ import boltons.debugutils
 boltons.debugutils.pdb_on_exception()
 
 from vispy import app, scene
+from vispy.color import get_colormap
 
 import numpy as np
 
@@ -17,13 +18,14 @@ def load_file():
 
 data = load_file()
 
-# create Vispy environment
+# create vispy environment
 canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
 view = canvas.central_widget.add_view()
 
-# attach visual node
-#TODO create volumes (data, clim, cmap) tuples
-volumes = []
+# attach data to visual node
+volumes = [
+    (data, None, get_colormap('GrBu'))
+]
 vol_node = MultiVolume(volumes, parent=view.scene)
 vol_node.transform = scene.STTransform(translate=(64, 64, 0))
 
