2ffaa6c546580f53cbba2d028e86158e99c9e77d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-03-08 18:58:47 +0800

    Update inheritance relationshiph of containers

--- utoolbox/container/registry.py
+++ utoolbox/container/registry.py
@@ -12,4 +12,29 @@ class ContainerRegistry(type):
         return cls
 
 class BaseContainer(metaclass=ContainerRegistry):
-    pass
+    def __init__(self, *args, resolution=None, **kwargs):
+        """
+        Parameters
+        ----------
+        resolution : tuple or list
+            Resolution of the unit elements in the container.
+        """
+        self.resolution = resolution
+
+    @property
+    def ndim(self):
+        raise NotImplementedError
+
+    @property
+    def resolution(self):
+        return self._resolution
+
+    @resolution.setter
+    def resolution(self, resolution):
+        if resolution is None:
+            resolution = tuple([1.] * self.ndim)
+        elif isinstance(resolution, list):
+            resolution = tuple(resolution)
+        elif not isinstance(resolution, tuple):
+            raise ValueError("invalid resolution parameter")
+        self._resolution = resolution
