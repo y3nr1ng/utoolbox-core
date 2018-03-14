abe54bb8d261bab4d72828440b8af2ff557a5f5f
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-03-14 23:46:22 +0800

    Wrap Chan-Vese as ROI analysis

---
+++ utoolbox/analysis/roi.py
@@ -0,0 +1,43 @@
+from types import GeneratorType
+
+from skimage.segmentation import find_boundaries
+
+from scipy.ndimage.morphology import binary_fill_holes
+
+from utoolbox.segmentation import chan_vese
+
+def _extract_mask(data, **kwargs):
+    phi = kwargs.pop('phi', 'checkerboard')
+    mask, phi, _  = chan_vese(
+        data,
+        mu=1e-2, tol=1e-4, max_iter=500, dt=1.,
+        init_level_set=phi
+    )
+    return binary_fill_holes(mask), phi
+
+def extract_mask(data, **kwargs):
+    """
+    Extract cellular contour of provided data.
+
+    It will try to yield over the data first.
+
+    Parameters
+    ----------
+    """
+    if isinstance(data, GeneratorType):
+        mask, phi = _extract_mask(next(data), **kwargs)
+        masks = [mask]
+        for d in data:
+            mask, phi = _extract_mask(d, phi=phi, **kwargs)
+            masks.append(mask)
+        return masks
+    else:
+        mask, _ = _extract_mask(data, **kwargs)
+        return mask
+
+def mask_to_contour(data):
+    """Thin wrapper to extract contour using skimage."""
+    if isinstance(data, list):
+        return [find_boundaries(m, mode='outer') for m in data]
+    else:
+        return find_boundaries(data, mode='outer')
