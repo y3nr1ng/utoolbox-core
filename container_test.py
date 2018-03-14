592133baa0ba4446f1e3b4880c0e8a0b57df73cc
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-03-14 23:46:54 +0800

    Refactor notebook as script

--- container_test.py
+++ container_test.py
@@ -1,3 +1,12 @@
+import logging
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+    '%(levelname).1s %(asctime)s [%(name)s] %(message)s', '%H:%M:%S'
+)
+handler.setFormatter(formatter)
+logging.basicConfig(level=logging.DEBUG, handlers=[handler])
+logger = logging.getLogger(__name__)
+
 import numpy as np
 from utoolbox.container import Volume
 
@@ -11,3 +20,11 @@ print("raw.resolution={}".format(raw.metadata.resolution))
 raw_dup = Volume(raw)
 print("raw_dup.shape={}, raw_dup.dtype={}".format(raw_dup.shape, raw_dup.dtype))
 print("raw_dup.resolution={}".format(raw_dup.metadata.resolution))
+
+print(raw_dup.metadata)
+
+print("raw.ndim={}".format(raw.ndim))
+
+raw_xy = np.amax(raw, axis=0)
+print("raw_xy.ndim={}".format(raw_xy.ndim))
+print("raw_xy.resolution={}".format(raw_xy.metadata.resolution))
