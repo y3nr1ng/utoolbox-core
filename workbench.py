dc62f4fff87e0dc0accf181e6706705ec79155d8
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-03-20 12:43:20 +0800

    Merge test scripts

--- workbench.py
+++
@@ -1,9 +0,0 @@
-from utoolbox.container import Volume
-
-mem_data = Volume("RFiSHp2aLFCYC/decon/488/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs_decon.tif")
-print(mem_data.shape)
-print(mem_data.dtype)
-
-from utoolbox.segmentation.slic import slic
-segments = slic(mem_data)
-print(segments.dtype)
