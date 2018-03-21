5c49f639300f5f345165d9df86b408529dedeb61
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-03-21 10:23:14 +0800

    Update internal data path

--- deskew_test.py
+++ deskew_test.py
@@ -1,6 +1,6 @@
 from utoolbox.container import Volume
 
-raw_data = Volume("data/RFiSHp2aLFCYC/raw/488/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs.tif")
+raw_data = Volume("data/20171201_RFiSHp2aLFCYC/raw/488/cell4_ch0_stack0000_488nm_0000000msec_0007934731msecAbs.tif")
 print(raw_data.shape)
 print(raw_data.dtype)
 
