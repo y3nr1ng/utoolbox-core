bfbda2da93f2c53f0c1cd3f4a88a840195ffb61b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-05-01 13:34:07 +0800

    Get 2D layered in PyCUDA running

--- workbench_data.py
+++ workbench_data.py
@@ -1,11 +1,13 @@
-import os
 import logging
+import os
 
+import imageio
 import numpy as np
 
 import utoolbox.utils.files as fileutils
 from utoolbox.container import Raster
 from utoolbox.container.layouts import Volume
+from utoolbox.transform import deskew
 
 #####
 
@@ -19,7 +21,7 @@ logger = logging.getLogger(__name__)
 
 #####
 
-source_folder = os.path.join(*["data", "20171201_RFiSHp2aLFCYC", "decon", "488"])
+source_folder = os.path.join(*["data", "20171201_RFiSHp2aLFCYC", "raw", "488"])
 file_list = fileutils.list_files(
     source_folder,
     name_filters=[
@@ -32,12 +34,11 @@ print("[0] = {}".format(file_list[0]))
 
 #####
 
-im1 = Raster(file_list[0], layout=Volume)
-im1_dup = Raster(im1, layout=Volume)
-im2 = Raster(shape=(128, 128))
-print(type(im1))
-print(im1._layout)
-print(type(im1_dup))
-print(im1_dup._layout)
-print(type(im2))
-print(im2._layout)
+im1 = Raster(file_list[0], layout=Volume, spacing=(.3, .102, .102))
+logger.debug(im1)
+logger.debug("im1.layout={}".format(im1.metadata.layout))
+
+#####
+
+im2 = deskew(im1, 30)
+imageio.volwrite("data/test.tif", im2)
