fb954929b5ae9dcf3571dbb4df7a1fd2e4686508
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-05-21 13:56:28 +0800

    Add system tray test

---
+++ systray_test.py
@@ -0,0 +1,26 @@
+import sys
+
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction, QApplication, QMenu, QSystemTrayIcon, QWidget, qApp
+
+class SystemTray(QSystemTrayIcon):
+    def __init__(self, parent):
+        super(SystemTray, self).__init__(parent)
+        self.setIcon(QIcon("disconnect.png"))
+
+        quit_action = QAction("Exit", self)
+        quit_action.triggered.connect(qApp.quit)
+
+        context_menu = QMenu()
+        context_menu.addAction(quit_action)
+
+        self.setContextMenu(context_menu)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    systray = SystemTray(w)
+    systray.show()
+
+    sys.exit(app.exec_())
