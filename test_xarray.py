980ea9466d2e955d281380fc704b644434caea8d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-07-15 19:52:54 +0800

    Adapt scipy example to imageio

--- test_xarray.py
+++ test_xarray.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import boltons.debugutils
+import imageio
 import numpy as np
 
 ###
@@ -18,10 +19,34 @@ logger = logging.getLogger(__name__)
 
 boltons.debugutils.pdb_on_exception()
 
+import utoolbox.io.imageio
+store = utoolbox.io.imageio.ImageioDataStore("data/deskew_sample2.tif")
 
-from utoolbox.io.spim import open_dataset
-ds = open_dataset("data/inventory", rescan=True)
+from xarray import Dataset, backends, conventions
+from xarray.backends.api import DATAARRAY_NAME, DATAARRAY_VARIABLE
+ds = conventions.decode_cf(
+            store, mask_and_scale=False, decode_times=False,
+            concat_characters=False, decode_coords=False,
+            drop_variables=None)
 
-print(ds)
+if len(ds.data_vars) != 1:
+    raise ValueError('Given file dataset contains more than one data '
+                     'variable. Please read with xarray.open_dataset and '
+                     'then select the variable you want.')
+else:
+    da, = ds.data_vars.values()
+
+da._file_obj = ds._file_obj
+
+# Reset names if they were changed during saving
+# to ensure that we can 'roundtrip' perfectly
+if DATAARRAY_NAME in ds.attrs:
+    da.name = ds.attrs[DATAARRAY_NAME]
+    del ds.attrs[DATAARRAY_NAME]
+
+if da.name == DATAARRAY_VARIABLE:
+    da.name = None
+
+print(da)
 
 raise RuntimeError
