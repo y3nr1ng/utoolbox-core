b3e99f13dfed1f0769bcd5297efc09c9bf9a11d2
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-09-04 10:46:42 +0800

    Refactor to PSF base class

--- utoolbox/simulate/psf/base.py
+++ utoolbox/simulate/psf/base.py
@@ -6,19 +6,29 @@ import numpy as np
 Resolution = namedtuple('Resolution', ['dxy', 'dz'])
 
 class PSF(object, metaclass=ABCMeta):
-    def __init__(self, resolution=(1, 1)):
+    def __init__(self, normalize='max', resolution=(1, 1)):
         if type(resolution) is not Resolution:
             try:
                 resolution = Resolution._make(resolution)
             except TypeError:
                 raise TypeError("invalid resolution input")
         self._resolution = resolution
-        self._buffer = None
+        self.normalize = normalize
 
     @abstractmethod
-    def __call__(self, wavelength):
+    def __call__(self, shape, wavelength, dtype):
         raise NotImplementedError
 
     @property
     def resolution(self):
         return self._resolution
+
+    @property
+    def normalize(self):
+        return self._normalize
+
+    @normalize.setter
+    def normalize(self, strategy):
+        if strategy not in ('none', 'energy', 'max'):
+            raise ValueError("invalid normalization strategy")
+        self._normalize = strategy
