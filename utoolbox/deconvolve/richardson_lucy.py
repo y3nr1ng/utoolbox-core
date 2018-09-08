0a3e069a15953c56b77b581eff8180f50e541bbc
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-09-08 18:56:12 +0800

    Find optimal FFT size

--- utoolbox/deconvolve/richardson_lucy.py
+++ utoolbox/deconvolve/richardson_lucy.py
@@ -5,6 +5,24 @@ underlying image that has been blurred by a known point spread function.
 import numpy as np
 import pyopencl as cl
 
+def is_optimal_size(n, factors=(2, 3, 5, 7)):
+    n = int(n)
+    assert n > 0, "size must be a positive integer"
+    for factor in factors:
+        while n % factor == 0:
+            n /= factor
+    return n == 1
+
+def find_optimal_size(target, prefer_pos=True):
+    if is_optimal_size(target):
+        return target
+    else:
+        for abs_delta in range(1, target):
+            sign = 1 if prefer_pos else -1
+            for delta in (sign*abs_delta, -sign*abs_delta):
+                candidate = target + delta
+                if is_optimal_size(candidate):
+                    return candidate
 
 class RichardsonLucy(object):
     def __init__(self, n_iter=10, shape=None):
