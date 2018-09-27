25fa12baa5123b359bed6c093663564e3a976919
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-09-27 14:08:36 +0800

    Functional IJ macro runner

--- utoolbox/util/decorator/macos.py
+++ utoolbox/util/decorator/macos.py
@@ -17,40 +17,43 @@ __all__ = [
 
 class AppDelegate(NSObject):
     def init(self):
-        """Designated initializer using NSObject."""
         self = objc.super(AppDelegate, self).init()
         if self is None:
             return None
 
-        # initializers must return self by pyobjc doc
+        self.wrapped_func = None
+        self.args, self.kwargs = None, None
+
+        # NOTE initializers must return self by pyobjc doc
         return self
 
-    #TODO fix this
-    def initWrappedFunc_(self, wrapped_func, *args, **kwargs):
-        self = self.init()
+    def applicationDidFinishLaunching_(self, notification):
+        self.performSelectorInBackground_withObject_("runWrappedFunc:", 0)
+
+    def runWrappedFunc_(self, arg):
+        self.wrapped_func()
+        # terminate explicitly, or it hangs when the wrapped code exits
+        NSApp().terminate_(self)
+
+class ConcreteAppDelegate(AppDelegate):
+    def initWrappedFunc_(self, wrapped_func):
+        self = objc.super(ConcreteAppDelegate, self).init()
         if self is None:
             return None
 
         self.wrapped_func = wrapped_func
-        self.args = args
-        self.kwargs = kwargs
+        # NOTE these are not expanded
+#        self.args = args
+#        self.kwargs = kwargs
 
         return self
-
-    def applicationDidFinishLaunching_(self, notification):
-        self.performSelectorInBackground_withObject_("run_wrapped_func:", 0)
-
-    def run_wrapped_func_(self, arg):
-        self.wrapped_func(*self.args, **self.kwargs)
-        # terminate explicitly, or it hangs when the wrapped code exits
-        NSApp().terminate_(self)
 
 def prelaunch_cocoa(func):
     def _launcher(*args, **kwargs):
         app = NSApplication.sharedApplication()
 
         # wrap the function in a delegate
-        delegate = AppDelegate.alloc().initWrappedFunc_(func, *args, **kwargs)
+        delegate = ConcreteAppDelegate.alloc().initWrappedFunc_(func)
         NSApp().setDelegate_(delegate)
 
         # sent keyboard events to the UI
