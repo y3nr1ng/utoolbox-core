3d1445507776448acb0f07edbdee1d63d26f2cae
Liu Yen-Ting <windows.linux.mac@gmail.com>
2018-10-08 11:24:43 +0800

    Dynamic macro building

--- tests/test_smlm.py
+++ tests/test_smlm.py
@@ -16,13 +16,16 @@ logger = logging.getLogger(__name__)
 
 
 ##### FETCH DATA #####
-
+src = [
+    '09142018_HalotagEGFPc3Nup153+PmeI/clone1_zp5um_100ms_interval_20s_partial/clone_ch1_stack0003_638nm_0060056msec_0001984925msecAbs.tif',
+    '09142018_HalotagEGFPc3Nup153+PmeI/clone1_zp5um_100ms_interval_20s_partial/clone_ch1_stack0000_638nm_0000000msec_0001924869msecAbs.tif',
+    '09142018_HalotagEGFPc3Nup153+PmeI/clone1_zp5um_100ms_interval_20s_partial/clone_ch1_stack0002_638nm_0040037msec_0001964906msecAbs.tif'
+]
 
 ##### EXCEUTE THUNDERSTORM #####
-
 # ThunderSTORM(ndim, cal_file=None, tmp_dir=None)
 #worker = ThunderSTORM(2, tmp_dir='/home2/scratch')
 worker = ThunderSTORM(2)
-worker.run('src_dir', 'dst_dir')
+worker.run(src, 'dst_dir')
 
 ##### RESULT #####
