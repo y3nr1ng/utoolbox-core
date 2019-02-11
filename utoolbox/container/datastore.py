72a40c8c934149907158f8a388b37c54624df88c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-02-11 13:02:52 +0800

    Create tar extractor

--- utoolbox/container/datastore.py
+++ utoolbox/container/datastore.py
@@ -5,50 +5,40 @@ Create datastore for large collections of data.
 
 from abc import ABCMeta, abstractmethod
 import glob
+import json
 import logging
+from lzma import LZMADecompressor
 import os
+import tarfile
+
+import xxhash
 
 __all__ = [
     'Datastore',
     'FileDatastore',
-    'ImageDatastore'
+    'ImageDatastore',
+    'TarDatastore'
 ]
 
 logger = logging.getLogger(__name__)
 
 class Datastore(object, metaclass=ABCMeta):
-    def __init__(self, location, sub_dir=False, pattern='*', extensions=None):
+    def __init__(self, read_func=None):
         """
         Parameters
         ----------
-        location : str or list of str
-            Files or folders to include in the datastore.
-        sub_dir : bool, default to False
-            Include subfolders within folder.
-        pattern : str
-            Patterns in the filename, default to '*'.
-        extensions : None or list of str
-            Extensions of files, select all if 'None'.
+        read_func : func
+            Function to perform the read operation.
         """
-        if sub_dir:
-            location = os.path.join(location, "**")
-
-        extensions = [pattern if extensions is None else extensions]
-        extensions = ["{}.{}".format(pattern, ext) for ext in extensions]
-
-        files = []
-        for ext in extensions:
-            pathname = os.path.join(location, ext)
-            files.extend(glob.glob(pathname, recursive=sub_dir))
-        self.files = files
-
-class FileDatastore(Datastore):
-    def __init__(self, location, read_func=None, **kwargs):
-        super(FileDatastore, self).__init__(location, **kwargs)
-        self._read_func = read_func
+        if read_func is None:
+            # nop
+            self._read_func = lambda x: x
+        else:
+            self._read_func = read_func
+        self._inventory = []
         self._index = 0
         self._read_size = 1
-
+        
     def __iter__(self):
         return self
 
@@ -61,7 +51,7 @@ class FileDatastore(Datastore):
     @property
     def has_data(self):
         """Determine if data is available to read."""
-        return self._index < len(self.files)
+        return self._index < len(self._inventory)
 
     @property
     def read_func(self):
@@ -78,16 +68,6 @@ class FileDatastore(Datastore):
         else:
             self._read_size = new_read_size
 
-    @property
-    def numpartitions(self):
-        """Number of datastore partitions."""
-        pass
-
-    def partition(self):
-        """Partition a datastore."""
-        #TODO mark by labeling
-        pass
-
     def preview(self):
         """Subset of data in datastore."""
         curr_index = self._index
@@ -100,17 +80,17 @@ class FileDatastore(Datastore):
         self._index += self.read_size
         if self.read_size > 1:
             # avoid overflow during batch read
-            n_files = len(self.files)
+            n_files = len(self._inventory)
             read_size = self.read_size
             if self._index > n_files:
                 read_size -= self._index-n_files
                 self._index = n_files
             return [
-                self.read_func(self.files[self._index-i-1])
+                self.read_func(self._inventory[self._index-i-1])
                 for i in reversed(range(read_size))
             ]
         else:
-            return self.read_func(self.files[self._index-1])
+            return self.read_func(self._inventory[self._index-1])
 
     def read_all(self):
         """Read all data in datastore.
@@ -120,14 +100,49 @@ class FileDatastore(Datastore):
         If all the data in the datastore does not fit in memory, then `readall`
         returns an error.
         """
-        self._index = len(self.files)
-        return [self.read_func(fp) for fp in self.files]
+        self._index = len(self._inventory)
+        return [self.read_func(fp) for fp in self._inventory]
 
     def reset(self):
         """Reset datastore to initial state."""
         self._index = 0
         self._read_size = 1
 
+class FileDatastore(Datastore):
+    def __init__(self, location, read_func=None, sub_dir=False, pattern='*', 
+                 extensions=None):
+        """
+        Parameters
+        ----------
+        location : str or list of str
+            Files or folders to include in the datastore.
+        read_func : func
+            Function to perform the read operation.
+        sub_dir : bool, default to False
+            Include subfolders within folder.
+        pattern : str
+            Patterns in the filename, default to '*'.
+        extensions : None or list of str
+            Extensions of files, select all if 'None'.
+        """
+        super(FileDatastore, self).__init__(read_func)
+
+        if sub_dir:
+            location = os.path.join(location, "**")
+
+        extensions = [pattern if extensions is None else extensions]
+        extensions = ["{}.{}".format(pattern, ext) for ext in extensions]
+
+        files = []
+        for ext in extensions:
+            pathname = os.path.join(location, ext)
+            files.extend(glob.glob(pathname, recursive=sub_dir))
+        self._inventory = files
+
+    @property
+    def files(self):
+        return self._inventory
+    
 class ImageDatastore(FileDatastore):
     supported_extensions = ("tif")
 
@@ -137,3 +152,72 @@ class ImageDatastore(FileDatastore):
         super(ImageDatastore, self).__init__(
             location, read_func=read_func, extensions=extensions, **kwargs
         )
+
+class TarDatastore(Datastore):
+    def __init__(self, root, read_func, mapped=False, verify=True, 
+                 memlimit=None):
+        if not root.lower().endswith('.tar'):
+            raise ValueError("not a TAR file")   
+        self._root = root
+
+        tar = tarfile.open(self.root, 'r:')
+        self._tar = tar
+        self._inventory = tar.getmembers()
+
+        self._decompressor = LZMADecompressor(
+            format=FORMAT_XZ, 
+            memlimit=memlimit
+        )
+        def _extract(tarinfo):
+            """Extract and decompress."""
+            data = self._tar.extractfile(tarinfo).read()
+            return self._decompressor.decompress(data)
+        if verify:
+            def _extract_and_verify(tarinfo):
+                """Extract, decompress, and verify."""
+                ori_digest = tarinfo.name
+                data = read(tarinfo)
+                ext_digest = xxhash.xxh64_hexdigest(data)
+                if ori_digest != ext_digest:
+                    raise HashMismatchError("hash mismatch after decompression")
+                return data
+            extract = _extract_and_verify
+        else:
+            extract = _extract
+        
+        def _read_func(x):
+            """Read from extracted data."""
+            return read_func(extract(x))
+        super(TarDatastore, self).__init__(_read_func)
+
+        try:
+            mapping = tar.getmember('mapping.json')
+        except KeyError:
+            if mapped:
+                raise UndefinedMappingError(
+                    "cannot find mapping in the archive"
+                )
+            # use default order
+            names = tar.getnames()
+            mapping = {k: "{:03d}".format(i) for i, k in enumerate(names)} 
+        
+        # TODO save filename re-matcher
+        
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._tar.close()
+
+    @property
+    def root(self):
+        return self._root
+
+class DatastoreError(Exception):
+    """Base class for datastore exceptions."""
+
+class UndefinedMappingError(DatastoreError):
+    """Undefine filename mapping for tar datastores."""
+
+class HashMismatchError(DatastoreError):
+    """Digest mismatch after file decompression."""
