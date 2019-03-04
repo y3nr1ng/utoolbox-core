9dc09c06f994e522420062e942350faf5993f1cc
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-03-04 19:54:19 +0800

    Update .gitignore for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -185,3 +185,6 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# -- Options for autodoc extension -------------------------------------------
+autodoc_member_order = 'groupwise'
