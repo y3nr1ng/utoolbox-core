b5c4ace50ff1c9734b20a3c26f486afa6281fd75
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-04-11 13:59:40 +0800

    Add fusion datastore test

---
+++ tests/_test_sandbox.py
@@ -0,0 +1,21 @@
+
+import coloredlogs
+import imageio 
+
+from utoolbox.container.datastore import (
+    ImageDatastore
+)
+
+coloredlogs.install(
+    level='DEBUG',
+    fmt='%(asctime)s  %(levelname)s %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+ds = ImageDatastore(
+    'GH146ACV_power100_60ms_z3_split_converted', 
+    imageio.imread
+)
+
+for k, v in ds.items():
+    print("{}, {}".format(k, v.shape))
