cd043621791b96e31b5a78176f51019d867c129d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-04-11 14:00:41 +0800

    Functional GPU/CPU RMLP hybrid

--- utoolbox/stitching/sandbox.py
+++ utoolbox/stitching/sandbox.py
@@ -1,6 +1,6 @@
-from collections import namedtuple
 import logging
-import uuid
+
+import networkx as nx
 
 __all__ = [
     'Sandbox'
@@ -8,69 +8,37 @@ __all__ = [
 
 logger = logging.getLogger(__name__)
 
-BoundingBox = namedtuple(
-    'BoundingBox', 
-    ['xmin', 'xmax', 'ymin', 'ymax', 'zmin', 'zmax']
-)
-
-class Brick(object):
-    def __init__(self, data, bbox, parent_id=None):
-        """
-        Parameters
-        ----------
-        data : np.ndarray
-            Represented data.
-        bbox : BoundingBox
-            Bounding box dimension in absolute coordinate.
-        parent : int
-            Integer ID of the parent.
-        """
-        assert data.ndim == 3
-        if len(bbox)/2 != data.ndim:
-            raise ValueError("bounding box dimension mismatch")
-        self._data = data
-        self._bbox = BoundingBox(*bbox)
-
-        self.parent_id = parent_id
-        self._id = uuid.uuid4().int>>64
-
-    @property
-    def bbox(self):
-        return self._bbox
-
-    @property
-    def data(self):
-        return self._data
-
-    @property
-    def id(self):
-        """A 64-bit random ID from UUID."""
-        return self._id
-
-
 class Sandbox(object):
-    def __init__(self):
-        pass
-    
-    def add(self, brick):
-        pass
-    
-    def remove(self, brick):
-        pass
-    
-    def consolidate(self, id=None):
-        """
-        Consolidate a specific brick in the sandbox, otherwise, all the bricks 
-        are consolidated. 
+    def __init__(self, ds):
+        self._datastore = ds
+        self._links = nx.Graph()
 
-        Parameter
-        ---------
-        id : int
-            ID of the brick to be consolidate, `None` to consolidate ALL.
-        
-        Note
-        ----
-        After consolidation occurs, no matter it is global or not, dimension of 
-        the sandbox will be fixed.
-        """
+        # populate the graph
+        self._links.add_nodes_from(ds.keys())
+
+    @property
+    def shape(self):
+        pass
+
+    @property
+    def datastore(self):
+        return self._datastore
+
+    def link(self, ref, target, shift, weight=1.):
+        self._links.add_edge(
+            ref, target, 
+            weight=weight, shift=shift
+        )
+    
+    def consolidate(self):
+        # build graph
+        mst = nx.minimum_spanning_tree(self._links)
+
+        #TODO determine bounding box
+
+        #TODO calculate shift list
+
+    def result(self, tiles=None):
+        #TODO determine overlaps
+
         pass
