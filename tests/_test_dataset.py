6af9699eb25d689569b6f1ca3431dc03644b2c40
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-05-08 17:49:28 +0800

    Combine new datastore format with SPIM dataset

--- tests/_test_dataset.py
+++ tests/_test_dataset.py
@@ -1,56 +1,33 @@
 # pylint: disable=W0612
 
 import os
+from pprint import pprint
 
-import pytest
-from pytest import fixture
+import coloredlogs
 
-import utoolbox.latticescope as llsm
-from utoolbox.latticescope import sort_by_timestamp
+from utoolbox.container.dataset import SPIMDataset
 
-@fixture
-def path():
-    test_dir = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(test_dir, "mock_dataset")
+coloredlogs.install(
+    level='DEBUG',
+    fmt='%(asctime)s %(module)s[%(process)d] %(levelname)s %(message)s',
+    datefmt='%H:%M:%S'
+)
 
-@fixture
-def ds(path):
-    return llsm.Dataset(path, refactor=False)
+test_dir = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(test_dir, "GH146ACV_power100_60ms_z3")
 
-def test_invalid_root():
-    with pytest.raises(FileNotFoundError) as _:
-        ds = llsm.Dataset('INVALID')
+dataset = SPIMDataset(path)
+pprint(dataset.metadata)
 
-def test_correct_read(path):
-    ds = llsm.Dataset(path, refactor=False)
+for ch, data in dataset.items():
+    print(" << {} >>".format(ch))
+    pprint(list(data.keys()))
 
-def test_correct_partitions(ds):
-    pass
-
-def test_generate_preview(ds):
-    pass
-
-
-if __name__ == '__main__':
-    from pprint import pprint
-
-    ##### LOAD FILE #####
-    path = "mock_dataset"
-    ds = llsm.Dataset(path, refactor=False)
-
-
-    ##### DUMP INVENTORY #####
-    pprint(ds.settings)
-
-    pprint(ds.datastore)
-    for k, v in ds.datastore.items():
-        print(" << {} >>".format(k))
-        pprint(v.files)
-
-
-    ##### SORT #####
-    sort_by_timestamp(ds)
-    pprint(ds.datastore)
-    for k, v in ds.datastore.items():
-        print(" << {} >>".format(k))
-        pprint(v.files)
+"""
+##### SORT #####
+sort_by_timestamp(ds)
+pprint(ds.datastore)
+for k, v in ds.datastore.items():
+    print(" << {} >>".format(k))
+    pprint(v.files)
+"""
--- utoolbox/container/dataset/base.py
+++ utoolbox/container/dataset/base.py
@@ -1,69 +1,64 @@
 # pylint: disable=undefined-variable
 
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from collections.abc import Mapping
 import os
 
-from .error import UndefinedConversionError
-
-class AbstractDataset(ABC):
+class Dataset(Mapping):
     def __init__(self, root):
         """
         :param str root: source of the dataset, flat layout
         """
         self._root = root
-        self._datastore = None
-    
-    @property
-    def datastore(self):
-        return self._datastore
-
-    @property
-    def root(self):
-        return self._root
-
-    @staticmethod
-    def from_dataset(ds, **kwargs):
-        """
-        Convert other form of dataset into current one.
-        
-        :param ds: source dataset
-        :type ds: :class:`.AbstractDataset`
-        """
-        raise UndefinedConversionError
-
-    @abstractmethod
-    def _generate_inventory(self):
-        raise NotImplementedError
-
-class AbstractMultiChannelDataset(AbstractDataset, Mapping):
-    def __init__(self, root):
-        super(AbstractMultiChannelDataset, self).__init__(root)
-        self._datastore = dict()
+        self._metadata = self._load_metadata()
+        self._datastore = self._load_datastore() 
 
     def __getitem__(self, key):
         return self._datastore[key]
     
     def __iter__(self):
-        """
-        .. note::
-            During iterations, we are actually iterate over the datastore.
-        """
         return self._datastore
     
     def __len__(self):
         return len(self._datastore)
 
     @property
-    def channels(self):
-        return self._datastore
-    
+    def metadata(self):
+        return self._metadata
+
     @property
-    def datastore(self):
-        """Block the usage to avoid confusion."""
-        raise NotImplementedError("use `channels` to retrieve datastores")
+    def root(self):
+        return self._root
+
+    def to_hdf(self, dst_root=None, virtual=True):
+        raise NotImplementedError
 
     @abstractmethod
-    def _map_channels(self):
-        """Map channels to datastore from dataset root."""
+    def _load_datastore(self):
+        raise NotImplementedError
+
+    def _load_metadata(self):
+        pass
+
+class MultiChannelDataset(Dataset):
+    def __init__(self, root):
+        super().__init__(root)
+    
+    def __iter__(self):
+        return iter(self._datastore)
+
+    @abstractmethod
+    def _find_channels(self):
         return NotImplementedError
+
+    @abstractmethod
+    def _load_channel(self, channel):
+        return NotImplementedError
+
+    def _load_datastore(self):
+        """Override for multi-channel setup."""
+        return {
+            channel: self._load_channel(channel)
+            for channel in self._find_channels()
+        }
+        
