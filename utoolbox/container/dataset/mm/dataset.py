b2584563501448aef86e394fe75b62068180b597
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-05-25 19:03:03 +0800

    Try out new formatter and linter

--- utoolbox/container/dataset/mm/dataset.py
+++ utoolbox/container/dataset/mm/dataset.py
@@ -3,18 +3,14 @@ import logging
 import os
 
 from ..base import MultiChannelDataset
-from .error import (
-    NoMetadataInTileFolderError,
-    NoSummarySectionError
-)
+from .error import NoMetadataInTileFolderError, NoSummarySectionError
 
 logger = logging.getLogger(__name__)
 
 
 class MicroManagerDataset(MultiChannelDataset):
-    """
-    Representation of Micro-Manager dataset stored in sparse stack format.
-    """
+    """Representation of Micro-Manager dataset stored in sparse stack format."""
+
     def __init__(self, root, tiled=True):
         """
         :param str root: source directory of the dataset
@@ -27,6 +23,7 @@ class MicroManagerDataset(MultiChannelDataset):
 
     @property
     def tiled(self):
+        """Whether dataset is composed of multiple tiles."""
         return self._tiled
 
     def _load_metadata(self):
@@ -41,19 +38,19 @@ class MicroManagerDataset(MultiChannelDataset):
                     break
             if path == self.root:
                 raise NoMetadataInTileFolderError()
-            logger.debug("using metadata from \"{}\"".format(path))
-        path = os.path.join(path, 'metadata.txt')
+            logger.debug('using metadata from "{}"'.format(path))
+        path = os.path.join(path, "metadata.txt")
 
-        with open(path, 'r') as fd:
+        with open(path, "r") as fd:
             metadata = json.load(fd)
         # return summary only, discard frame specific info
         try:
-            return metadata['Summary']
+            return metadata["Summary"]
         except KeyError:
             raise NoSummarySectionError()
 
     def _find_channels(self):
-        return self.metadata['ChNames']
+        return self.metadata["ChNames"]
 
     def _load_channel(self, channel):
         pass
