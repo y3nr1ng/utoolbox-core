360aeacf48a3dc4f2b38db2533df681f1849f65a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-05-28 14:38:48 +0800

    Add local data source deducer

--- utoolbox/io/root.py
+++ utoolbox/io/root.py
@@ -2,7 +2,6 @@
 Guess where the data directory is on this computer.
 """
 
-
 # retrieve module path
 import inspect
 import os
@@ -10,4 +9,4 @@ import os
 import utoolbox
 
 module_path = inspect.getmodule(utoolbox).__path__._path[0]
-DATA_ROOT = os.path.join(module_path, 'data')
+DATA_ROOT = os.path.join(module_path, "data")
