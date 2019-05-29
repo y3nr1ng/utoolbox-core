059c4b03b4c5d3d3872961fd458698a2b25b3bfc
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-05-29 09:15:51 +0800

    Update read the docs configuration

--- tests/_test_dataset_spim.py
+++ tests/_test_dataset_spim.py
@@ -8,9 +8,7 @@ from utoolbox.container.dataset import SPIMDataset
 
 logging.getLogger("tifffile").setLevel(logging.ERROR)
 coloredlogs.install(
-    level="DEBUG",
-    fmt="%(asctime)s %(module)s[%(process)d] %(levelname)s %(message)s",
-    datefmt="%H:%M:%S",
+    level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
 )
 
 dataset = SPIMDataset("~/Documents/Sinica (Data)/Projects/Wen-Chen/20180807/cell5")
@@ -19,6 +17,5 @@ pprint(dataset.metadata)
 
 for ch, data in dataset.items():
     print(" << {} >>".format(ch))
-    pprint(list(data._uri.items())[:10])
-
-    break
+    for i, array in enumerate(list(data._uri.values())[:5]):
+        print("[{}], {}, {}".format(i, array.shape, array.dtype))
