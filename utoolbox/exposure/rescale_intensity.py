3d36a8e26e2c2c06fd8fd8ba5d156162b160cf30
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-06-22 14:40:31 +0800

    Update rescale intensity to GPU only

--- utoolbox/exposure/rescale_intensity.py
+++ utoolbox/exposure/rescale_intensity.py
@@ -10,36 +10,28 @@ logger = logging.getLogger(__name__)
 
 class RescaleIntensity(object):
     """
-    Return image after stretching or shrinking its intensity levels. The algorithm is 
+    Return image after stretching or shrinking its intensity levels. The algorithm is
     based on the implementation in `skimage`_.
 
-    .. _skimage: 
+    .. _skimage:
         https://scikit-image.org/docs/dev/api/skimage.exposure.html
     """
 
     def __call__(self, data, in_range="image", out_range="dtype"):
-        on_device, dtype = isinstance(data, cp.ndarray), data.dtype
-        data = cp.asarray(data)
-
         im, iM = RescaleIntensity.as_min_max(data, in_range)
         om, oM = RescaleIntensity.as_min_max(data, out_range, clip_neg=(im >= 0))
 
         data = data.clip(im, iM)
 
         @cp.fuse
-        def _normalize(data, m, M):
-            return (data - m) / (M - m)
+        def _ops(data, im, iM, om, oM):
+            # normalize
+            data = (data - im) / (iM - im)
+            # scale to range
+            data = data * (oM - om) + om
+            return data
 
-        data = _normalize(data, im, iM)
-
-        @cp.fuse
-        def _scale_to_range(data, m, M):
-            return data * (M - m) + m
-
-        data = _scale_to_range(data, om, oM)
-
-        data = data if on_device else cp.asnumpy(data)
-        return data.astype(dtype)
+        return _ops(data, im, iM, om, oM)
 
     @staticmethod
     def as_min_max(data, range_value, clip_neg=False):
@@ -53,30 +45,31 @@ class RescaleIntensity(object):
             clip_neg (bool, optional): If True, clip the negative range. 
         
         Returns:
-            :rtype: (float, float)
+            :rtype: (cp.float32, cp.float32)
 
         Raises:
             TypeError: If this function cannot resolve the intensity range pair.
         """
         if range_value == "image":
-            m, M = data.min(), data.max()
-            m, M = m.astype(cp.float32), M.astype(cp.float32)
-        elif range_value == "dtype":
-            dtype = np.dtype(data.dtype).type
-            if issubclass(dtype, np.integer):
-                info = np.iinfo(dtype)
-                m, M = info.min, info.max
-            elif issubclass(dtype, np.floating):
-                m, M = -1.0, 1.0
-            else:
-                raise TypeError("unknown data type")
-            m = 0 if clip_neg else m
-            m, M = cp.float32(m), cp.float32(M)
+            m, M = np.asscalar(data.min()), np.asscalar(data.max())    
         else:
-            # force extraction tuple
-            m, M = range_value
-            m, M = cp.float32(m), cp.float32(M)
-        # guarantee they are floats
+            if isinstance(range_value, tuple):
+                # force extraction tuple
+                m, M = range_value
+            else:
+                if range_value == "dtype":
+                    dtype = np.dtype(data.dtype).type
+                else:
+                    dtype = range_value
+                if issubclass(dtype, np.integer):
+                    info = np.iinfo(dtype)
+                    m, M = info.min, info.max
+                elif issubclass(dtype, np.floating):
+                    m, M = -1.0, 1.0
+                else:
+                    raise TypeError("unknown data type")
+                m = 0 if clip_neg else m
+        m, M = np.float32(m), np.float32(M)
         return m, M
 
 
@@ -123,20 +116,24 @@ if __name__ == "__main__":
     """
 
     import imageio
-    image = imageio.volread('cell_in.tif')
-    print(image.shape)
+    from numpy.testing import assert_array_almost_equal
+
+    image = imageio.volread("cell_in.tif")
+    print("{}, {}".format(image.shape, image.dtype))
 
     from utoolbox.util.decorator import timeit
-    
+
     @timeit
     def cpu(image):
         from skimage.exposure import rescale_intensity
+
         for _ in range(10):
             result = rescale_intensity(image, out_range=(0, 1))
             result, image = image, result
         return image
+
     result1 = cpu(image)
-    
+
     @timeit
     def gpu(image):
         rescale_intensity = RescaleIntensity()
@@ -145,11 +142,8 @@ if __name__ == "__main__":
             result = rescale_intensity(image, out_range=(0, 1))
             result, image = image, result
         return cp.asnumpy(image)
+
     result2 = gpu(image)
+    imageio.volwrite("cell_out.tif", result2)
 
-    mempool = cp.get_default_memory_pool()
-    pinned_mempool = cp.get_default_pinned_memory_pool()
-    print(mempool.used_bytes())              
-    print(mempool.total_bytes())             
-    print(pinned_mempool.n_free_blocks())    
-
+    assert_array_almost_equal(result1, result2)
