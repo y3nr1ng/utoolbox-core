50c45bf09d9a377e622abc4637406ab8655f8ad3
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-06-29 17:10:45 +0800

    Remove derpecated test

--- tests/_test_dataset_mm.py
+++ tests/_test_dataset_mm.py
@@ -1,18 +1,56 @@
-import os
 from pprint import pprint
 
 import coloredlogs
+import imageio
+import numpy as np
 
-from utoolbox.container.dataset import MicroManagerDataset
+# import objgraph
+
+from utoolbox.data.dataset import MicroManagerDataset
+from utoolbox.data.datastore import VolumeTilesDatastore
 
 coloredlogs.install(
-    level='DEBUG',
-    fmt='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%H:%M:%S'
+    level="DEBUG", fmt="%(asctime)s  %(levelname)s %(message)s", datefmt="%H:%M:%S"
 )
 
-test_dir = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(test_dir, "mm_dataset")
 
-dataset = MicroManagerDataset(path)
-pprint(dataset.metadata)
+class DummyImage(object):
+    def __init__(self, path, shape=(32, 32), dtype=np.uint16):
+        self._path = path
+        self._data = np.empty(shape, dtype)
+
+    @property
+    def dtype(self):
+        return self._data.dtype
+
+    @property
+    def path(self):
+        return self._path
+
+    @property
+    def shape(self):
+        return self._data.shape
+
+
+def dummy_read(path):
+    return DummyImage(path)
+
+ds = MicroManagerDataset("perfuse_lectin594_poststain_lectin647_5")
+pprint(ds.metadata)
+
+with VolumeTilesDatastore(
+    "perfuse_lectin594_poststain_lectin647_5",
+    read_func=dummy_read,
+    folder_pattern="1-Pos_*",
+    file_pattern="*_561_*",
+    tile_shape=(3, 4),
+    return_as="plane",
+) as ds:
+    pprint(ds._uri)
+    # pprint(ds.metadata)
+
+    # print("\n=== objgraph ===")
+    # objgraph.show_growth(limit=30)
+    # print()
+
+# objgraph.show_growth(limit=30)
