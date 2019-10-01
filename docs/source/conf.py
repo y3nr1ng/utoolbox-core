5538cb646b7ff884b0e2b06d54de10fd5bce600b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-01 18:32:54 +0800

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2019, Liu, Yen-Ting"
 author = "Liu, Yen-Ting"
 
 # The short X.Y version
-version = "0.4.0.dev"
+version = "0.5.0.dev"
 # The full version, including alpha/beta/rc tags
-release = "0.4.0.dev"
+release = "0.5.0.dev"
 
 
 # -- General configuration ---------------------------------------------------
