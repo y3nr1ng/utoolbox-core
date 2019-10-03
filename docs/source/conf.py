8d47472d05d94b44975410ac441d440c63e8e334
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-03 18:22:06 +0800

    "Bump version from 0.5.14.dev to 0.5.15.dev"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2019, Liu, Yen-Ting"
 author = "Liu, Yen-Ting"
 
 # The short X.Y version
-version = "0.5.14.dev"
+version = "0.5.15.dev"
 # The full version, including alpha/beta/rc tags
-release = "0.5.14.dev"
+release = "0.5.15.dev"
 
 
 # -- General configuration ---------------------------------------------------
