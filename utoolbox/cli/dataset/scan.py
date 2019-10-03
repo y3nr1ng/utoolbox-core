f692c036e8dfb69b0d8ed91032bab6db2edb92a5
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-03 17:14:44 +0800

    Fix channel selector for single channel

--- utoolbox/cli/dataset/scan.py
+++ utoolbox/cli/dataset/scan.py
@@ -26,6 +26,7 @@ def determine_format(path):
     else:
         raise RuntimeError("unable to determine dataset flavor")
 
+    # select channel to work with
     if len(ds.info.channels) > 1:
         questions = [
             {
@@ -35,17 +36,19 @@ def determine_format(path):
                 "choices": ds.info.channels,
             }
         ]
-        answers = prompt(questions)
+        channel = prompt(questions)['channel']
+    else:
+        channel = ds.info.channels[0]
 
-        with ds[answers["channel"]] as source:
-            i = 0
-            for key, value in source.items():
-                logger.info(f".. {key}")
+    with ds[channel] as source:
+        i = 0
+        for key, value in source.items():
+            logger.info(f".. {key}")
 
-                # DEBUG
-                if i < 5:
-                    i += 1
-                else:
-                    break
+            # DEBUG
+            if i < 5:
+                i += 1
+            else:
+                break
 
-                yield value
+            yield value
