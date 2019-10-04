5c995ece9ec9f84ddc468abba29254ca9a2adb89
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-04 09:15:29 +0800

    Fix versioning glitch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2019, Liu, Yen-Ting"
 author = "Liu, Yen-Ting"
 
 # The short X.Y version
-version = "0.5.15"
+version = "0.5.15.dev1"
 # The full version, including alpha/beta/rc tags
-release = "0.5.15"
+release = "0.5.15.dev1"
 
 
 # -- General configuration ---------------------------------------------------
