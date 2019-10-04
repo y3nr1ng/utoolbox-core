377a9c11022c39d6f6cddbea9e4b5c604bfcbe8f
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-04 14:53:59 +0800

    "Bump version from 0.5.20.dev2 to 0.5.20.dev3"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2019, Liu, Yen-Ting"
 author = "Liu, Yen-Ting"
 
 # The short X.Y version
-version = "0.5.20.dev2"
+version = "0.5.20.dev3"
 # The full version, including alpha/beta/rc tags
-release = "0.5.20.dev2"
+release = "0.5.20.dev3"
 
 
 # -- General configuration ---------------------------------------------------
