c88924d8fff11eee9925bb91a1d750207cf3017c
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-04 14:53:55 +0800

    Use partial load (maybe?)

--- utoolbox/data/dataset/mm/dataset.py
+++ utoolbox/data/dataset/mm/dataset.py
@@ -46,7 +46,7 @@ class MicroManagerDataset(MultiChannelDataset):
         try:
             with open(meta_path, "r") as fd:
                 # discard frame specific info
-                return json.load(fd, strict=False)["Summary"]
+                return json.loads(fd.read(), encoding='ascii', strict=False)["Summary"]
         except KeyError:
             raise NoSummarySectionError()
 
