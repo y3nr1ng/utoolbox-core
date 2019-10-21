84aacf0a026bde290731cb97181ee9cbf76b7726
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-10-21 13:07:16 +0800

    "Bump version from 0.5.27.dev1 to 0.5.27.dev2"

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     #   bumpversion release
     # to next version
     #   bump patch/minor/major
-    version="0.5.27.dev1",
+    version="0.5.27.dev2",
     # one-line description for the summary field
     description="A Python image processing package for LLSM.",
     long_description=long_description,
