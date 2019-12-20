9522f00d7ba23d4077da006c205b31fc77f6cf1d
Liu Yen-Ting <windows.linux.mac@gmail.com>
2019-12-20 09:35:52 +0800

    Add missing voxel size function

--- workspace/test_terastitcher_dataset.py
+++ workspace/test_terastitcher_dataset.py
@@ -1,10 +1,10 @@
 import coloredlogs
 
-from utoolbox.io.dataset import TeraStitcherDatasetError
+from utoolbox.io.dataset import TeraStitcherDataset
 
 coloredlogs.install(
     level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
 )
 
-ds = TeraStitcherDatasetError("/scratch/20191217_16_50_20_cerebellum_tile/")
+ds = TeraStitcherDataset("/scratch/20191217_16_50_20_cerebellum_tile/")
 print(ds.inventory)
