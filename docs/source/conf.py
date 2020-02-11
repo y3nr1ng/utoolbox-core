8224348ce748d04e2ac766730ec5538ea93ea420
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-02-11 16:39:54 +0800

    "Bump version from 0.6.1.dev2 to 0.6.1"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ copyright = "2019, Liu, Yen-Ting"
 author = "Liu, Yen-Ting"
 
 # The short X.Y version
-version = "0.6.1.dev2"
+version = "0.6.1"
 # The full version, including alpha/beta/rc tags
-release = "0.6.1.dev2"
+release = "0.6.1"
 
 
 # -- General configuration ---------------------------------------------------
