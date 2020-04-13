971b91483d67bea3fcdfe0d8a71801a4593ad946
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-04-13 16:53:12 +0800

    Reset version

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     #   bumpversion release
     # to next version
     #   bump patch/minor/major
-    version="0.6.4",
+    version="0.0.0",
     # one-line description for the summary field
     description="A Python image processing package for LLSM.",
     long_description=long_description,
