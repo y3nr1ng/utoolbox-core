8522cf0651c1e5ae4ae7b6c8df0950ebb1bb7e1e
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-04-15 19:08:30 +0800

    Add abstract method for zarr dataset

--- utoolbox/io/dataset/zarr/dataset.py
+++ utoolbox/io/dataset/zarr/dataset.py
@@ -8,4 +8,44 @@ logger = logging.getLogger("utoolbox.io.dataset")
 
 
 class ZarrDataset(DenseDataset, MultiChannelDataset, MultiViewDataset, TiledDataset):
-    pass
+    def __init__(self, root_dir):
+        pass
+
+    ##
+
+    @property
+    def read_func(self):
+        pass
+
+    @property
+    def root_dir(self):
+        return self._root_dir
+
+    ##
+
+    def _can_read(self):
+        pass
+
+    def _enumerate_files(self):
+        pass
+
+    def _load_array_info(self):
+        pass
+
+    def _load_channel_info(self):
+        pass
+
+    def _load_metadata(self):
+        pass
+
+    def _load_tiling_coordinates(self):
+        pass
+
+    def _load_view_info(self):
+        pass
+
+    def _load_voxel_size(self):
+        pass
+
+    def _retrieve_file_list(self, coord_dict):
+        pass
