5e6bb01b3d4ba4434388ad6d546685431cc0ad5b
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-07-09 15:12:24 +0800

    "Bump version from 0.0.23.dev3 to 0.0.23.dev4"

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     #   bumpversion release
     # to next version
     #   bump patch/minor/major
-    version="0.0.23.dev3",
+    version="0.0.23.dev4",
     # one-line description for the summary field
     description="Core functions for uToolbox.",
     long_description=long_description,
